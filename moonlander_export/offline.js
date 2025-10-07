a150bc82f0d6ffc4dfa58765eb260212bff94511
xbuster17 <xbuster17@gmail.com>
2025-10-06 22:40:20 -0300

    construct test

---
+++ moonlander_export/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1759801035,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/fire-sheet0.png",
+		"images/fire-sheet1.png",
+		"images/ground-sheet0.png",
+		"images/cabin-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/start_bs-sheet0.png",
+		"images/cabin_col-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
